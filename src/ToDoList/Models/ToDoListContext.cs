130dc066df16a4be8cf3eac6ee99489365102b68
newellmatt <mnewell10@gmail.com>
2016-04-18 09:33:34 -0700

    Add more files

---
+++ src/ToDoList/Models/ToDoListContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Data.Entity;
+
+namespace ToDoList.Models
+{
+    public class ToDoListContext : DbContext
+    {
+        public virtual DbSet<Item> Items { get; set; }
+        protected override void OnConfiguring (DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=ToDoList;integrated security = True");
+        }
+    }
+}
